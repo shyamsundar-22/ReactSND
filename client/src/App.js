aea5fe25df3d45b115667b3f8d0171daec535fc1
568418 <apr-2019>
2020-02-22 13:18:27 +0530

    react fe integration

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      {/* <h1>HHHHHH</h1> */}
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
